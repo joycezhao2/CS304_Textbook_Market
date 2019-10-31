a223f34859bc414f8ec241fafbde2e70215972f7
apan18 <39341195+apan18@users.noreply.github.com>
2019-10-30 21:49:07 -0400

    Add files via upload

--- database.sql
+++ database.sql
@@ -8,12 +8,12 @@ drop table if exists A_book_course;
 
 /* for all users, either a buyer or a seller */
 create table users (
-    id varchar(20) primary key,
+    username varchar(20) NOT NULL primary key,
     name varchar(50),
     email varchar(100),
-    phnum char(10)
-)
-engine = InnoDB;
+    phnum varchar(10),
+    )
+    engine = InnoDB;
 
 /* for all courses differentiated by deparment, section and professor */
 create table courses (
@@ -22,33 +22,47 @@ create table courses (
     number char(3),
     professor varchar(50),
     primary key (CRN, professor)
-)
-engine = InnoDB;
+    )
+    engine = InnoDB;
 
 /* for the abstract book, each is associated with one course */
 create table A_books (
-    id int NOT NULL AUTO_INCREMENT primary key,
+    id int auto_increment primary key,
     title varchar(50),
-    author varchar(30)
-)
-engine = InnoDB;
+    author varchar(30),
+    )
+    engine = InnoDB;
 
 /* for each book uploaded by the seller */
 create table S_books (
-    id int NOT NULL AUTO_INCREMENT primary key,
-    foreign key (A_book) references A_books(id),
-    foreign key (seller) references users(id),
-    foreign key (buyer) references users(id),
+    id int auto_increment NOT NULL primary key,
     price int,
     sold_status BIT,
-    condition varchar(20) CHECK (condition in ('Brand New','Like New',
-                                                'Very Good','Good','Acceptable'))
-)
-engine = InnoDB;
+    condition varchar(20),
+    book int,
+    seller varchar(20),
+    buyer varchar(20),
+    index (book),
+    index (seller),
+    index (buyer),
+    foreign key (book) references A_books(id),
+    foreign key (seller) references users(username),
+    foreign key (buyer) references users(username)
+        on delete CASCADE
+        on update CASCADE
+    )
+    engine = InnoDB;
 
 /* associates an A_book with a course */
 create table A_book_course(
-    foreign key (book) references A_books(id),
-    foreign key (CRN) references courses(CRN),
+    book int,
+    CRN char(5),
+    index (book),
+    index (CRN),
     primary key (book, CRN)
-)
+    foreign key (book) references A_books(id),
+    foreign key (CRN) references courses(CRN)
+        on delete CASCADE
+        on update CASCADE
+    )
+    engine = InnoDB;
