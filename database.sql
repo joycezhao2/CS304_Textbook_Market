032add7f20050048d4b6eaf1f125e8790257454f
joyce <textbookmarket304@tempest>
2019-11-24 14:32:06 -0500

    upated database sql file

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-use ypan2_db;
+use textbooks_db;
 
 drop table if exists users;
 drop table if exists courses;
@@ -11,9 +11,9 @@ create table users (
     username varchar(20) NOT NULL primary key,
     name varchar(50),
     email varchar(100),
-    phnum varchar(10),
-    )
-    engine = InnoDB;
+    phnum varchar(10)
+)
+engine = InnoDB;
 
 /* for all courses differentiated by deparment, section and professor */
 create table courses (
@@ -22,16 +22,16 @@ create table courses (
     number char(3),
     professor varchar(50),
     primary key (CRN, professor)
-    )
-    engine = InnoDB;
+)
+engine = InnoDB;
 
 /* for the abstract book, each is associated with one course */
 create table A_books (
     id int auto_increment primary key,
     title varchar(50),
-    author varchar(30),
-    )
-    engine = InnoDB;
+    author varchar(30)
+)
+engine = InnoDB;
 
 /* for each book uploaded by the seller */
 create table S_books (
@@ -42,27 +42,18 @@ create table S_books (
     book int,
     seller varchar(20),
     buyer varchar(20),
-    index (book),
-    index (seller),
-    index (buyer),
     foreign key (book) references A_books(id),
     foreign key (seller) references users(username),
     foreign key (buyer) references users(username)
-        on delete CASCADE
-        on update CASCADE
-    )
-    engine = InnoDB;
+)
+engine = InnoDB;
 
 /* associates an A_book with a course */
 create table A_book_course(
     book int,
     CRN char(5),
-    index (book),
-    index (CRN),
-    primary key (book, CRN)
     foreign key (book) references A_books(id),
-    foreign key (CRN) references courses(CRN)
-        on delete CASCADE
-        on update CASCADE
-    )
-    engine = InnoDB;
+    foreign key (CRN) references courses(CRN),
+    primary key (book, CRN)
+)
+engine = InnoDB;
