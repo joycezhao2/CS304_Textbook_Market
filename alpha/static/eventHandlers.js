936f20009c82d3786a941a1f85a3d93d559e5746
joyce <textbookmarket304@tempest>
2019-12-07 11:36:08 -0500

    Alice's update

---
+++ alpha/static/eventHandlers.js
@@ -0,0 +1,19 @@
+// JavaScript File to add event handlers
+/* global $ sendInfo progressive_enhancement_on */
+
+$('#dept').on("click",function() {
+    if (progressive_enhacement) {
+        event.precentDefault();
+
+        var dept = $(this).text();
+        
+        $.post(URL,{'dept':dept},update,'json');
+    }
+})
+
+function update(obj) {
+    console.log(obj)
+
+    if(obj.error) {
+        $('#errors').empty().html('Error:'+obj.err);
+}
