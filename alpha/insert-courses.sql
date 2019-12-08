9c73b1514ce8c5b4615603491a475bb04950b84a
joyce <textbookmarket304@tempest>
2019-12-08 16:36:39 -0500

    insert-courses.sql update

--- alpha/insert-courses.sql
+++ alpha/insert-courses.sql
@@ -1,7 +1,8 @@
+
 use textbooks_db;
 
 load data local infile 'courses.csv' 
 into table courses
 fields terminated by ',' 
-(department, number)
-lines terminated by '\n';
+lines terminated by '\n'
+(department, number);
